772c4534faf814512d3c9dd1db23ada76be742e1
Dan Best <dilshoddilmurodov799@gmail.com>
2025-12-26 23:01:19 +0500

    ready for API test

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,17 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import path from 'path'
+import { connectDB } from './lib/db.js'
 
 
 dotenv.config()
 
-const PORT = process.env.PORT || 3000;
-
 const app = express()
 const __dirname = path.resolve()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()) // req.body
 
 app.use("/api/auth", authRoutes)    
 app.use("/api/messages", messageRoutes)    
@@ -27,6 +30,9 @@ if (process.env.NODE_ENV === "production") {
     })
 }
 
-app.listen(PORT, () => console.log("Server running on port "+ PORT))
+app.listen(PORT, () => {
+    console.log("Server running on port "+ PORT)
+    connectDB()
+})
 
 
